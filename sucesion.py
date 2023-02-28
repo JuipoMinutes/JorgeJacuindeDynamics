e08ed2dae49df2bcd609d6f236e2eb6b188256da
Jorge Jacuinde Mayés <111296990+JuipoMinutes@users.noreply.github.com>
2023-02-28 11:55:46 -0600

    Update sucesion.py

--- sucesion.py
+++ sucesion.py
@@ -4,17 +4,22 @@ print("Aqui el comando de latex:")
 
 print(R"\begin{equation} \frac{1}{10^1} ",end=" ")
 
-
+expresion ="0.1 "
 
 for x in range(2,n+1):
     print(R"+ \frac{1}{10^"+str(x)+R"} ",end="")
+    expresion = expresion + "+ " + str(1/(10**x)) + " "
 
 print (R"\end{equation}")
-
-print("El numero resultante es:")
 
 total = 0
 for x in range(1,n+1):
     total += 1/(10**x)
+
+print("Expresion: ")
+
+print(expresion)
+
+print("El numero resultante es:")
 
 print(total)
