21ac3d17c9ad9815a52c280f871b3b1b8cb201b7
Jorge Jacuinde Mayés <111296990+JuipoMinutes@users.noreply.github.com>
2023-02-28 11:59:34 -0600

    Update sucesion.py

--- sucesion.py
+++ sucesion.py
@@ -7,19 +7,11 @@ print(R"\begin{equation} \frac{1}{10^1} ",end=" ")
 expresion ="0.1 "
 
 for x in range(2,n+1):
-    print(R"+ \frac{1}{10^"+str(x)+R"} ",end="")
-    expresion = expresion + "+ " + str(1/(10**x)) + " "
+    print(R", \frac{1}{10^"+str(x)+R"} ",end="")
+    expresion = expresion + ", " + str(1/(10**x)) + " "
 
 print (R"\end{equation}")
-
-total = 0
-for x in range(1,n+1):
-    total += 1/(10**x)
 
 print("Expresion: ")
 
 print(expresion)
-
-print("El numero resultante es:")
-
-print(total)
